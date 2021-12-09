964d0dbfbbfd207b3eed5cef6111cec83229fd67
notmeegoreng <terencegoh.y.c@gmail.com>
2021-12-09 13:57:31 +0800

    trying to get bottom nav to work (and failing)

--- app/src/main/java/com/example/appventure_hack_2021/MainActivity.kt
+++ app/src/main/java/com/example/appventure_hack_2021/MainActivity.kt
@@ -2,20 +2,45 @@ package com.example.appventure_hack_2021
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.appventure_hack_2021.databinding.ActivityMainBinding
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 
-const val RC_SIGN_IN = 1
+private const val RC_SIGN_IN = 1
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        // Choose authentication providers
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_map, R.id.nav_home, R.id.nav_settings
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        navView.selectedItemId = R.id.nav_home
+
         val providers = arrayListOf(
             AuthUI.IdpConfig.GoogleBuilder().build()
         )
@@ -32,19 +57,20 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == RC_SIGN_IN) {
-            val response = IdpResponse.fromResultIntent(data)
+        when(requestCode) {
+            RC_SIGN_IN -> {
+                val response = IdpResponse.fromResultIntent(data)
 
-            if (resultCode == Activity.RESULT_OK) {
-                // Successfully signed in
-                val user = FirebaseAuth.getInstance().currentUser
-                val usernameTextView: TextView = findViewById(R.id.username)
-                usernameTextView.text = user?.displayName
-                // ...
-            } else {
-                println(response?.error)
+                if (resultCode == Activity.RESULT_OK) {
+                    // Successfully signed in
+                    val user = FirebaseAuth.getInstance().currentUser
+                    // val usernameTextView: TextView = findViewById(R.id.username)
+                    // usernameTextView.text = user?.displayName
+                    // ...
+                } else {
+                    println(response?.error)
+                }
             }
         }
     }
-
 }
