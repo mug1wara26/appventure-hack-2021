cd3cae4c18a9315d023155cdebac70050af38f96
notmeegoreng <terencegoh.y.c@gmail.com>
2021-12-10 21:52:23 +0800

    Add a stats cardview to historyfragment

--- app/src/main/java/com/example/appventure_hack_2021/models/HistoryRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/appventure_hack_2021/models/HistoryRecyclerViewAdapter.kt
@@ -11,8 +11,23 @@ import java.time.LocalDateTime
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
+private const val stats_id = 0
 class HistoryRecyclerViewAdapter(private val histories: List<History>, private val context: Context) : RecyclerView.Adapter<HistoryRecyclerViewAdapter.ViewHolder>()  {
     class ViewHolder(private val view: View, private val context: Context) : RecyclerView.ViewHolder(view) {
+        fun bindStats(histories: List<History>) {
+            val offset = ZoneOffset.ofHours(context.resources.getInteger(R.integer.tz_offset))
+            val formatter = DateTimeFormatter.ofPattern("")
+            view.findViewById<TextView>(R.id.content).text = context.getString(
+                R.string.history_stats_text,
+                histories.size,
+                histories.sumOf { it.totalDistance.toDouble() },
+                LocalDateTime.ofEpochSecond(
+                    histories.sumOf { it.startTime - it.endTime } * 1000,
+                    0, offset
+                ).format(formatter)
+            )
+            return
+        }
         fun bind(history: History) {
             view.findViewById<TextView>(R.id.header).text = context.getString(
                 R.string.history_start_stop_text, history.startName, history.startName
@@ -34,15 +49,24 @@ class HistoryRecyclerViewAdapter(private val histories: List<History>, private v
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
-        LayoutInflater.from(parent.context).inflate(R.layout.cardview_history_fragment, parent, false),
-        context
-    )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val id = if (viewType == 0) R.layout.cardview_history_stats else R.layout.cardview_history_fragment
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(id, parent, false),
+            context
+        )
+    }
 
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(histories[position])
+        if (getItemViewType(position) == stats_id) {
+            holder.bindStats(histories)
+            return
+        }
+        holder.bind(histories[position - 1])
     }
 
-    override fun getItemCount(): Int = histories.size
+    override fun getItemCount(): Int = histories.size + 1
+
+    override fun getItemViewType(position: Int): Int = if (position == 0) stats_id else 1
 }
