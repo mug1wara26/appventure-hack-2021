fd789a462d33deafe2a983a0b27385396617ef80
notmeegoreng <terencegoh.y.c@gmail.com>
2021-12-12 13:25:31 +0800

    change GetDataFromRTDB to func

--- app/src/main/java/com/example/appventure_hack_2021/GetDataFromRTDB.kt
+++ app/src/main/java/com/example/appventure_hack_2021/GetDataFromRTDB.kt
@@ -6,24 +6,14 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 
-open class GetDataFromRTDB(
-    private val databaseRef: DatabaseReference,
-    private val listener: GetDataListener
-) {
-    interface GetDataListener {
-        fun onSuccess(snapshot: DataSnapshot)
-    }
+fun getDataFromRTDB(databaseRef: DatabaseReference, listener: (DataSnapshot) -> Unit) {
+    databaseRef.addListenerForSingleValueEvent(object: ValueEventListener{
+        override fun onDataChange(snapshot: DataSnapshot) {
+            listener(snapshot)
+        }
 
-    fun getData(){
-        databaseRef.addListenerForSingleValueEvent(object: ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                listener.onSuccess(snapshot)
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-               Log.e("Database Error", error.details)
-            }
-
-        })
-    }
+        override fun onCancelled(error: DatabaseError) {
+            Log.e("Database Error", error.details)
+        }
+    })
 }
